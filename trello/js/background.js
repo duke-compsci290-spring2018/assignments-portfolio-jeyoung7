ad6ba0d747092985ac7727256cbc8bc6df4e168a
Jacob <jey13@duke.edu>
2018-03-06 17:46:33 -0500

    here we go

--- trello/js/background.js
+++
@@ -1,9 +0,0 @@
-new Vue({
-    el: '#board',
-    data: {
-        description: 'Type any colour name, rgb, hex or hsl in the input below and see what happens!',
-        bgc: {
-            backgroundColor: ''
-        }
-    }
-})
