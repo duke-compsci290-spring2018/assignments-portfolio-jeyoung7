b4888d6e0f56018d450d80fb6e8eafcb63f171b1
Jacob <jey13@duke.edu>
2018-03-16 12:48:27 -0400

    midway

--- trello/js/cleantrello.js
+++ trello/js/cleantrello.js
@@ -1,55 +1,3 @@
-var todoList = [
-    {
-        title: 'Download code',
-        completed: false,
-        about: "hello",
-        id: 1,
-        seen: false,
-
-    },
-    {
-        title: 'Study code',
-        completed: false,
-        about: "hello",
-        id: 1,
-        seen: false,
-
-    },
-    {
-        title: 'Finish code',
-        completed: false,
-        about: "hello",
-        id: 1,
-        seen: false,
-
-    }
-];
-var todoList2 = [
-    {
-        title: 'Download code',
-        completed: false,
-        about: "hello",
-        id: 1,
-        seen: false,
-    },
-    {
-        title: 'Study code',
-        completed: false,
-        about: "hello",
-        id: 1,
-        seen: false,
-
-    },
-    {
-        title: 'Finish code',
-        completed: false,
-        about: "hello",
-        id: 1,
-        seen: false,
-
-    }
-];
-
 var config = {
     apiKey: "AIzaSyABq904mMuJqgVB-9spHtkMhGW2Cf-Wz7E",
     authDomain: "my-website-3e3ca.firebaseapp.com",
@@ -61,25 +9,9 @@ var config = {
 
 var db = firebase.initializeApp(config).database();
 var fireLists = db.ref('lists');
-var lsts =[];
-fireLists.once('value',function(snap) {
-    snap.forEach(function(item) {
-        var itemVal = item.val();
-        lsts.push(itemVal);
-    });
 
-});
 Vue.use(VueFire);
-console.log(lsts);
-var store = {
-    state: {
-        lists: fireLists
 
-
-
-    }
-
-}
 
 var users = [
     {
@@ -98,11 +30,6 @@ var users = [
 
     },
 ];
-todoList.title ="One";
-todoList2.title ="Two";
-
-todoList2.edit =false;
-
 
 var filters = {
     all: function (todos) {
@@ -116,29 +43,30 @@ var filters = {
     }
 }
 var App = new Vue({
-    data() {
-        return {
-            lists: lsts,
-            newTodo: '',
-            visibility: 'all',
-            showModal: false,
-            users: users,
-            currentIndex: 0,
-            currentText: '',
-        }
+    data: {
+        newTodo: '',
+        visibility: 'all',
+        showModal: false,
+        users: users,
+        currentIndex: 0,
+        currentText: '',
+
+    },
+    firebase: {
+        lists: fireLists,
     },
     computed: {
         // return todos that match the currently selected filter
 
 
         // return count of the remaining active todo items
-       /* remaining (list) {
-            return filters.active(this.lists.list).length;
-        }*/
+        /* remaining (list) {
+             return filters.active(this.lists.list).length;
+         }*/
     },
     methods: {
 
-        //frlass lecture
+        //class lecture easy way to change backgroundcolor
         changeBackground: function (event) {
             document.body.style.backgroundColor = event.target.value;
         },
@@ -146,44 +74,25 @@ var App = new Vue({
             this.visibility = filter;
         },
         addList: function () {
-         newList = [];
-         newList.title = this.currentText;
-         this.lists.push(newList);
-         fireLists.push(newList);
-        this.currentText='';
+            newList = [];
+            newList.title = this.currentText;
+            newList.selected = false;
+            fireLists.push(newList);
+
+            this.currentText = '';
         },
-        // add newly entered todo item if it exists and clear it to prepare for the next one
-        addTodo(list) {
-            this.newTodo = this.newTodo.trim();
-            console.log(list['.key']);
-            if (this.newTodo) {
-                this.list.push({
-                    title: this.newTodo,
-                    completed: false
-                });
-                fireLists.ref(list.key).push({ title: this.newTodo,
-                    completed: false});
-                // text input displays this value, so clear it to indicate ready to type a new one
-                this.newTodo = '';
+        checkboxToggle(list,key) {
+            if
+            fireLists.child(list['.key']).child('todos').child(key).remove();
 
-
-            }
         },
-
-
         // remove given todo from the list
-        removeTodo(list,todo) {
-            console.log(list);
-            console.log(todo);
-            list.splice(list.indexOf(todo), 1)
+        removeTodo(list, key) {
+            fireLists.child(list['.key']).child('todos').child(key).remove();
         },
-        deleteList(list){
-
-            console.log(list);
-            this.lists.splice(this.lists.indexOf(list), 1)
-
+        deleteList(list) {
+            fireLists.child(list['.key']).remove();
         },
-        // remove all completed todos from the list
 
         mounted() {
             console.log(this.$el);
@@ -194,13 +103,12 @@ var App = new Vue({
 });
 Vue.component('modal', {
     template: '#modal-template',
-    props: ['show','index'],
-    data: function () {
+    props: ['show', 'index', 'list'],
+    data: () => {
         return {
             title: '',
             about: '',
-            selected: '',
-
+            selected: false,
             users: users
         };
     },
@@ -209,14 +117,20 @@ Vue.component('modal', {
             this.$emit('close');
             this.title = '';
             this.about = '';
+            this.selected = '';
 
         },
         savePost: function () {
             console.log(this.index);
-            //this.list.push({title: this.title, about: this.about, completed: false, seen: false, assigned: this.selected });
+            console.log(this.list);
 
-            //fireLists.ref(list).push({title: this.title, about: this.about, completed: false, seen: false, assigned: this.selected });
-
+            fireLists.child(this.list['.key']).child('todos').push({
+                title: this.title,
+                about: this.about,
+                selected: this.selected,
+                completed: false,
+                seen: false,
+            })
             this.close();
 
         }
