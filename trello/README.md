7947308e98aca30cdd588eac0d09ff823edb4f80
Jacob <jey13@duke.edu>
2018-03-20 22:54:00 -0400

    Version 2.0

--- trello/README.md
+++ trello/README.md
@@ -9,20 +9,46 @@ Date Finished:  TBD
 Students Consulted:
 Dina Daas
 
-Hours: 25 hours
+Hours: 30 hours
+
+
+
 
 Resources Used: 
 Vue From Lab from Lab
 
 https://vuejs.org/v2/examples/modal.html
+Used to be baseline for my Post create modal
 
 https://adamwathan.me/2016/01/04/composing-reusable-modal-dialogs-with-vuejs/
 
-https://jsfiddle.net/jpeter06/ppyeo1tg/
 https://www.w3schools.com/bootstrap/bootstrap_images.asp
+Used to format my images better and for my image gallery at the bottom of the dialgoue
 
-Bugs: A few: you can't comment wihtout hte modal closing right after. 
+https://csscompressor.com/
+Used to clean up and comrpess my CSS to make it more compact and useful.
+
+Bugs: 
+-Adding an Image on the Modal will cause it to close, but it does work
+-Adding a Comment on the Modal will cause it to close, but it does work
+
 
 Other Info:
-This is an almost finished trello. It neeeds a few more features and to clean up a bit. The code is super messy and I will work on it tomororw.
- 
+This is the finished Trello. I turned it in late to add extra functionality such as the activity log and add styling.
+The challenges implemented are: Make any number comments on a card, View Project Activities, add multiple assignees to a card.
+I put a ton of effort into the functionality side, CSS side lesser so. But, I believe it is decent looking and definitely took some effort to get it here.
+
+For the functionality:
+Brief walk through to get started:
+
+1. Sign Up
+2. Sign In  (no authentication)
+3. Add Category 
+4. Add a List
+5. Add New post
+6. View 
+- click on card to see it enlarged, to edit it, to add comments, images , etc.. 
+- click button menu to change category 
+- click color button to change things up
+7. View activity
+below cards click view activity to see the activity log
--- trello/js/cleantrello.js
+++ trello/js/cleantrello.js
@@ -6,32 +6,30 @@ var config = {
     storageBucket: "my-website-3e3ca.appspot.com",
     messagingSenderId: "260939014953"
 };
+//INITIALIZING FIREBASE
 
 var db = firebase.initializeApp(config).database();
 var fireLists = db.ref('lists');
 var userList = db.ref('Users');
 var categoryList = db.ref('categories');
+var activityList = db.ref('pastActivity');
+
 var storageRef = firebase.storage().ref(); //storage @DUVALL
 
 Vue.use(VueFire);
 
-
-
-
-var filters = {
-    all: function (todos) {
-        return todos;
-    },
-    active: function (todos) {
-        return todos.filter(todo => !todo.completed);
-    },
-    completed: function (todos) {
-        return todos.filter(todo => todo.completed);
+////https://stackoverflow.com/questions/1484506/random-color-generator used to generate category colors
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
     }
+    return color;
 }
-
+////used for getting a random color
 var App = new Vue({
-    data: {
+    data: { //central data store
         newTodo: '',
         filteredList: this.lists,
         showModal: false,
@@ -39,6 +37,22 @@ var App = new Vue({
         user: {
             uid: '',
             name: '',
+            email: '',
+            imageURL: '',
+        },
+        generalCategories: {
+            date: {
+                title: 'date', color: '#B21287'
+            },
+            title: {
+                title: 'date', color: '#FF00BA'
+            },
+            about: {
+                title: 'date', color: '#14CCCC'
+            },
+            assigned: {
+                title: 'assigned', color: '#34B2B2'
+            },
         },
         newCategory: '',
         comment: '',
@@ -46,9 +60,10 @@ var App = new Vue({
         currentText: '',
         signUpModal: false,
         signInModal: false,
+        showActivity: false,
         newTitle: '',
         currentName: '',
-        newSubTodo:'',
+        newSubTodo: '',
         edit: false,
         form: {
             name: '',
@@ -61,28 +76,22 @@ var App = new Vue({
     firebase: {
         lists: fireLists,
         users: userList,
-        categories: categoryList
+        categories: categoryList,
+        pastActivity: activityList,
     },
-    computed: {
-        // return todos that match the currently selected filter
-
-
-        // return count of the remaining active todo items
-        /* remaining (list) {
-             return filters.active(this.lists.list).length;
-         }*/
-    },
+    computed: {},
     methods: {
 
         //class lecture easy way to change backgroundcolor
         filter: function (category) {
             var self = this;
-            fireLists.once('value', function(snapshot) {
+            fireLists.once('value', function (snapshot) {
                 snapshot.forEach(function (snapshot) {
                     snapshot.child('todos').forEach(function (todoref) {
                         var todo = todoref.val();
-                        if (todo.category != category.title) {
+                        if (todo.category.title != category.title) {
                             fireLists.child(snapshot.key).child('todos').child(todoref.key).child('visible').set(false);
+
                         }
                         else {
                             fireLists.child(snapshot.key).child('todos').child(todoref.key).child('visible').set(true);
@@ -95,72 +104,68 @@ var App = new Vue({
         },
         all: function () {
             var self = this;
-            fireLists.once('value', function(snapshot) {
+            fireLists.once('value', function (snapshot) {
                 snapshot.forEach(function (snapshot) {
                     snapshot.child('todos').forEach(function (todoref) {
                         var todo = todoref.val();
 
-                            fireLists.child(snapshot.key).child('todos').child(todoref.key).child('visible').set(true);
+                        fireLists.child(snapshot.key).child('todos').child(todoref.key).child('visible').set(true);
 
 
                     });
                 });
             });
 
-        },
+        },  //allthefunctional methods
         addCategory() {
-            categoryList.push({title: this.newCategory});
-            console.log(this.newCategory)
+            categoryList.push({title: this.newCategory, color: getRandomColor()});
+            console.log(this.newCategory);
 
-            this.newCategory='';
+            this.newCategory = '';
         },
         changeBackground: function (event) {
             document.body.style.backgroundColor = event.target.value;
         },
-        addSubTodo: function (list,key)  {
+        addSubTodo: function (list, key) {
             fireLists.child(list['.key']).child('todos').child(key).child('tasks').push({
                 title: this.newSubTodo
             });
+            this.addToActivity(this.newSUBTODO+this.user.name, "SUBTODO Added");
+
             this.newSubTodo = '';
 
-        },
-        addComment: function(list,todo,key) {
+        }, //addcomment
+
+        addComment: function (list, todo, key) {
             console.log(firebase.database.ServerValue.TIMESTAMP);
+            this.addToActivity(todo.title, "COMMENT ADDED");
+
             fireLists.child(list['.key']).child('todos').child(key).child('comments').push({
                 content: this.comment,
                 createdAt: firebase.database.ServerValue.TIMESTAMP,
                 author: this.user.name,
             });
-            todo.seen = true;
 
         },
-        removeSubTodo: function(list,key,task) {
+//savepost
+        save: function (list, key, todo) {
 
+            this.addToActivity(todo.title, "TODO EDITED");
 
-            fireLists.child(list['.key']).child('todos').child(key).child('tasks').child(task).remove();
-            event.preventDefault()
-        }
-        ,
-        save: function(list,key,todo) {
-            console.log(todo);
-            console.log(key);
-            if (!todo.images)
-            {
+            if (!todo.images) {
                 todo.images = [];
             }
-            if (!todo.tasks)
-            {
+            if (!todo.tasks) {
                 todo.tasks = [];
             }
-            if (!todo.comments)
-            {
+            if (!todo.comments) {
                 todo.comments = [];
             }
             fireLists.child(list['.key']).child('todos').child(key).set({
                 title: todo.title,
                 about: todo.about,
                 deadline: todo.deadline,
-                assigned: {name: todo.assigned.name, email: todo.assigned.email},
+                assigned: todo.assigned,
                 images: todo.images,
                 tasks: todo.tasks,
                 completed: todo.completed,
@@ -173,15 +178,27 @@ var App = new Vue({
             todo.seen = true;
             this.edit = false;
         },
+        addToActivity: function (title, action) {
+
+
+            activityList.push({
+                title: title,
+                action: action,
+                createdAt: firebase.database.ServerValue.TIMESTAMP,
+                author: this.user.name,
+            });
+        }
+        ,
         setFilter(filter) {
             this.visibility = filter;
-        },
+        }, //addlist
         addList: function () {
             newList = [];
             newList.title = this.currentText;
             newList.selected = false;
             newList.expand = true;
 
+            this.addToActivity(newList.title, "LIST ADDED");
             fireLists.push(newList);
 
             this.currentText = '';
@@ -192,10 +209,14 @@ var App = new Vue({
             fireLists.child(list['.key']).child('todos').child(key).remove();
 
         },
-        removeTodo(list, key) {
+        removeTodo(todo,list, key) {
+            this.addToActivity(todo.title, "TODO REMOVED");
+
             fireLists.child(list['.key']).child('todos').child(key).remove();
         },
         deleteList(list) {
+            this.addToActivity(list.title, "LIST REMOVED");
+
             fireLists.child(list['.key']).remove();
         },
         close: function () {
@@ -209,23 +230,31 @@ var App = new Vue({
         mounted() {
             console.log(this.$el);
         },
+        assign: function() {
+            if (this.assigned ) {
+                this.assignedList.push(this.assigned);
+            }
+        },
         closePop(todo) {
             this.$emit('close');
 
             todo.seen = false;
 
         },
-        addToCard: function(list,key) {
+        addToCard: function (list, key) {
+            this.addToActivity(todo.title, "TODO ADDED");
+//adds image to existing card
+
             var input = document.getElementById('newImage');
             var temp = this;
             // have all fields in the form been completed
             var fileName = '';
-            if (this.newTitle.length > 0  && input.files.length > 0) {
+            if (this.newTitle.length > 0 && input.files.length > 0) {
                 var file = input.files[0];
                 // get reference to a storage location and
                 var fileAddress = storageRef.child('images/' + file.name)
                     .put(file)
-                    .then(snapshot =>  db.ref('lists').child(list['.key']).child('todos').child(key).child('images').push({
+                    .then(snapshot => db.ref('lists').child(list['.key']).child('todos').child(key).child('images').push({
                         title: this.newTitle,
                         url: snapshot.downloadURL,
                     }).catch(error => console.log(error)));
@@ -234,13 +263,20 @@ var App = new Vue({
                 input.value = '';
             }
         },
-        removeImageLink: function(list,key,index,todo) {
-           db.ref('lists').child(list['.key']).child('todos').child(key).child('images').child(index).remove();
+        removeImageLink: function (list, key, index, todo, event) {
+            this.addToActivity(todo.title, "IMAGE REMOVED");
+//removes image from existing card
+            db.ref('lists').child(list['.key']).child('todos').child(key).child('images').child(index).remove();
+            if (event) {
+                event.preventDefault()
+            }
+            todo.seen = true;
+
         },
-        moveList: function(index,direction) {
+        moveList: function (index, direction) {
 
-
-            if ((this.lists.length-1 > index && direction === 1) || (index>0 && direction === -1)) {
+//moveslist left and right
+            if ((this.lists.length - 1 > index && direction === 1) || (index > 0 && direction === -1)) {
 
                 var temp = this.lists[index];
                 var tempRight = this.lists[index + direction];
@@ -265,10 +301,14 @@ var App = new Vue({
             }
 
         },
-        moveTodo: function(todo,key, index,direction) {
-            if ((this.lists.length-1 > index && direction === 1) || (index>0 && direction === -1)) {
+        moveTodo: function (todo, key, index, direction) {
+//moves todoelft and right
+            if ((this.lists.length - 1 > index && direction === 1) || (index > 0 && direction === -1)) {
 
-                db.ref('lists').child(this.lists[index+direction]['.key']).child('todos').push(todo);
+                var action = todo.title+' moved from '+this.lists[index].title+' to '+this.lists[index+direction].title;
+                    this.addToActivity(todo.title, action);
+
+                db.ref('lists').child(this.lists[index + direction]['.key']).child('todos').push(todo);
                 db.ref('lists').child(this.lists[index]['.key']).child('todos').child(key).remove();
             }
         },
@@ -283,60 +323,58 @@ var App = new Vue({
                 // get reference to a storage location and
                 storageRef.child('images/' + file.name)
                     .put(file)
-                    .then(snapshot =>  db.ref('Users').push({name: name, email: email, imageURL: snapshot.downloadURL}))
+                    .then(snapshot => db.ref('Users').push({name: name, email: email, imageURL: snapshot.downloadURL}))
                     .catch(error => console.log(error));
                 // reset input values so user knows to input new data
                 input.value = '';
             }
             this.signUpModal = false;
             this.close();
-
         },
         signIn: function (event) {
             var th = this;
-            if (this.form.name.length> 0) {
+//note on sign in, all it does is change active user which affects activity log and a little style
                 db.ref('Users').orderByChild('name').equalTo(this.form.name).on("value", function (snapshot) {
-                    console.log(snapshot.val());
-                    if (snapshot){
-                        console.log(snapshot.val());
-                        snapshot.forEach(function(data) {
+                    if (snapshot) {
+                        snapshot.forEach(function (data) {
                             th.user.name = data.val().name;
                             th.user.uid = data.key;
-                            console.log(data);
+                            th.user.imageURL = data.val().imageURL;
                         });
                     }
                     else {
                         alert("doesn't exist");
                     }
                 });
-            }
-            else if (this.form.email.length>0) {
+
+
                 db.ref('Users').orderByChild('email').equalTo(this.form.email).on("value", function (snapshot) {
-                    if (snapshot){
+                    if (snapshot) {
                         console.log(snapshot.val());
-                        snapshot.forEach(function(data) {
+                        snapshot.forEach(function (data) {
                             th.user.name = data.val().name;
                             th.user.uid = data.key;
-                            console.log(data);
+                            th.user.imageURL = data.val().imageURL;
+
                         });
                     } else {
                         alert("doesn't exist");
                     }
                 });
+
+            if (this.user.name.length=== 0) {
+                alert("doesn't exist");
+
             }
-
-
-            console.log(th.user.name);
-                    th.signUpModal = false;
-                    th.close();
-
+            th.signUpModal = false;
+            th.close();
 
 
         },
     }
 
 });
-
+// USED FOR MY POST FORM
 Vue.component('modal', {
     template: '#modal-template',
     props: ['show', 'index', 'list'],
@@ -346,18 +384,20 @@ Vue.component('modal', {
             about: '',
             deadline: "yyyy-MM-dd",
             selected: false,
-            assigned: {
-                name: '',
-                email: ''
-            },
-            imgTitle:'',
-            newImageTitle:'',
+            assigned : {name: '',
+            email: ''},
+            assignedList: [],
+            imgTitle: '',
+            newImageTitle: '',
             images: [],
-            category: '',
+            category: {
+                title: '',
+                color: '#FFFFFF',
+            },
         };
     },
     firebase: {
-      users: userList,
+        users: userList,
         categories: categoryList
     },
     methods: {
@@ -366,16 +406,28 @@ Vue.component('modal', {
             this.title = '';
             this.about = '';
             this.images = {},
-            this.assigned = {};
-            this.deadline= "MM-dd-yyyy";
-            this.category='';
+
+
+                this.assigned = [];
+            this.deadline = "MM-dd-yyyy";
+            this.category = {title: '',
+                color: '',};
         },
-        addImage: function() {
+        assign: function() {
+            if (this.assigned ) {
+                this.assignedList.push({
+                  name: this.assigned.name,
+                email: this.assigned.email,});
+            }
+        },
+        addImage: function () {
             var input = document.getElementById('entryFile');
             var temp = this;
             // have all fields in the form been completed
+            console.log(this.newImageTitle);
             var fileName = '';
-            if (this.newImageTitle.length > 0  && input.files.length > 0) {
+            if (this.newImageTitle.length > 0 && input.files.length > 0) {
+                console.log("hello");
                 var file = input.files[0];
                 // get reference to a storage location and
                 var fileAddress = storageRef.child('images/' + file.name)
@@ -386,32 +438,52 @@ Vue.component('modal', {
                 input.value = '';
             }
         },
-        addToList (title, url) {
+        addToList(title, url) {
             this.images.push({
                 title: title,
                 url: url
             });
             this.newImageTitle = '';
         },
+        addToActivity: function (title, action) {
+
+            console.log(this.assignedList);
+
+            activityList.push({
+                title: title,
+                action: action,
+                createdAt: firebase.database.ServerValue.TIMESTAMP,
+                author: this.assigned.name,
+            });
+        },
         savePost: function () {
+            this.addToActivity(this.title,"CARD ADDED");
             console.log(this.index);
             console.log(this.list);
 
-            console.log(this.category);
+            if (!this.category) {
+                this.category.title = '';
+                this.category.color = '#FFFFFF';
 
+            }
+            if (!this.assignedList) {
+                this.assignedList = [];
 
+            }
+
+            console.log(this.assignedList);
             fireLists.child(this.list['.key']).child('todos').push({
                 title: this.title,
                 about: this.about,
                 deadline: this.deadline,
-                assigned: { name: this.assigned.name, email: this.assigned.email },
+                assigned: this.assignedList,
                 images: this.images,
                 tasks: [],
                 completed: false,
                 seen: false,
                 comments: [],
                 visible: true,
-                category: this.category.title,
+                category: {title: this.category.title, color: this.category.color}
             });
 
             this.close();
